e861ca6736e856e10f191d48d513242ba68f0326
akshit <akshitk540@gmail.com>
2023-12-30 14:05:24 +0530

    added basic controllers

--- src/app.js
+++ src/app.js
@@ -13,4 +13,13 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,9 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.staus(error.code || 500).json({
-      success: false,
-      message: err.message,
-    });
-  }
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) =>
+      next(err, next)
+    );
+  };
 };
 
 export { asyncHandler };
