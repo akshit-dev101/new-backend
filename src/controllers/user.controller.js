8e6b02e1cfe9e55cd4a383b0fbc065b45af9726f
akshit <akshitk540@gmail.com>
2024-01-11 07:10:59 +0530

    handeld errors

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,7 +21,7 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
-  console.log(req.files);
+  // console.log(req.files);
 
   const avatarLocalPath = req?.files?.avatar[0]?.path;
   let coverImageLocalPath;
